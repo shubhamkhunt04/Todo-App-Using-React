d555b0799e1bcd45553b6c739845b8a456099d11
Mohit Sojitra <mohitsojitra0@gmail.com>
2021-10-06 17:30:24 +0530

    code optimizatrion added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoList from "./Component/TodoList";
-import Header from "./Component/Header";
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoList from './Component/TodoList'
+import Header from './Component/Header'
 
 function App() {
   return (
     <div>
       <div className="container">
-      <Header />
-      <TodoList />
+        <Header />
+        <TodoList />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,89 +1,88 @@
-import React, { useState } from "react";
+import React, {useMemo, useState} from 'react'
 
 function Todolist() {
-  let [task, setTask] = useState("");
+  let [task, setTask] = useState('')
 
-  let [todo, setTodo] = useState([]);
+  let [todo, setTodo] = useState([])
 
-  const inputChangeHandler = (e) => {
+  const inputChangeHandler = e => {
     setTask({
       id: todo.length,
       title: e.target.value,
       isCompleted: false,
-    });
-  };
+    })
+  }
 
-  const btnHandler = (e) => {
-    e.preventDefault();
-    setTodo([...todo, task]);
-    setTask({ title: "" });
-  };
+  const btnHandler = e => {
+    e.preventDefault()
+    setTodo([...todo, task])
+    setTask({title: ''})
+  }
 
-  const deleteBtnHandler = (itemid) => {
-    let newTodoList = todo.filter((item) => {
-      return item.id !== itemid;
-    });
-    setTodo(newTodoList);
-  };
+  const deleteBtnHandler = itemid => {
+    let newTodoList = todo.filter(item => {
+      return item.id !== itemid
+    })
+    setTodo(newTodoList)
+  }
 
   const deleteAllCompletedBtnHandler = () => {
-    let newTodoList = todo.filter((item) => {
-      console.log(item.isCompleted);
-      return item.isCompleted === false;
-    });
-    console.log(newTodoList);
-    setTodo(newTodoList);
-  };
+    let newTodoList = todo.filter(item => {
+      console.log(item.isCompleted)
+      return item.isCompleted === false
+    })
+    console.log(newTodoList)
+    setTodo(newTodoList)
+  }
 
-  const isCompletedHandler = (itemid) => {
-    let newTodo1 = todo.filter((item) => {
-      return item.id === itemid;
-    });
+  const isCompletedHandler = itemid => {
+    let newTodo1 = todo.filter(item => {
+      return item.id === itemid
+    })
 
-    let newTodo2 = todo.filter((item) => {
-      return item.id !== itemid;
-    });
+    let newTodo2 = todo.filter(item => {
+      return item.id !== itemid
+    })
 
-    newTodo1[0].isCompleted = !newTodo1[0].isCompleted;
-    let todoData = [...newTodo2, ...newTodo1];
-    setTodo(todoData);
-    console.log(todoData);
-  };
+    newTodo1[0].isCompleted = !newTodo1[0].isCompleted
+    let todoData = [...newTodo2, ...newTodo1]
+    setTodo(todoData)
+    console.log(todoData)
+  }
 
   // not complete
   const markAsAllCompletedBtnHandler = () => {
-    let newTodo = todo.filter((item) => {
-      return (item.isCompleted = true);
-    });
-    console.log(newTodo);
-  };
+    let newTodo = todo.filter(item => {
+      return (item.isCompleted = true)
+    })
+    console.log(newTodo)
+  }
   // not complete
   const markAsAllInCompletedBtnHandler = () => {
-    let newTodo = todo.map((item) => {
-      return (item.isCompleted = false);
-    });
-    console.log(newTodo);
-  };
+    let newTodo = todo.map(item => {
+      return (item.isCompleted = false)
+    })
+    console.log(newTodo)
+  }
 
-  
-  const getStyle = (flag) => {
+  const getStyle = useMemo(flag => {
     return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      background: "#f4f4f4",
-      textDecoration: flag ? "line-through" : "none",
-    };
-  };
-  const btnStyle = () => {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      background: '#f4f4f4',
+      textDecoration: flag ? 'line-through' : 'none',
+    }
+  }, [])
+  const btnStyle = useMemo(() => {
     return {
-      background: "#ff0000",
-      color: "#ffffff",
-      cursor: "pointer",
-      border: "none",
-      float: "right",
-      padding: "5px 9px",
-    };
-  };
+      background: '#ff0000',
+      color: '#ffffff',
+      cursor: 'pointer',
+      border: 'none',
+      float: 'right',
+      padding: '5px 9px',
+    }
+  }, [])
 
   return (
     <div>
@@ -107,27 +106,20 @@ function Todolist() {
           value="DeleteAllCompleted"
           onClick={deleteAllCompletedBtnHandler}
         />
-
-        
       </form>
 
       {/* display todo  */}
 
-      {todo.map((item) => {
-        console.log("Rendering start");
+      {todo.map(item => {
+        console.log('Rendering start')
         return (
           <div key={item.id} style={getStyle(item.isCompleted)}>
             <input
               type="checkbox"
               onChange={isCompletedHandler.bind(this, item.id)}
-            />{" "}
+            />{' '}
             {item.title}
             {/* edit todos */}
-            {/* <input
-              type="button"
-              value="edit"
-              onClick={editBtnHandler.bind(this, item)}
-            /> */}
             {/* delete a single todo */}
             <input
               type="button"
@@ -136,10 +128,10 @@ function Todolist() {
               style={btnStyle()}
             />
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
 
-export default Todolist;
+export default Todolist
